c6b62ab9775d8ef639aabd9de3d6f944cc7b4e79
Sreeju KS <sreesreejume@gmail.com>
2021-09-25 23:03:29 +0530

    Update ------ 1. indent and some comments added.

--- Stock_alert.py
+++ Stock_alert.py
@@ -1,15 +1,20 @@
 # Import required modules
 from lxml import html
 import requests
-import pandas as pd
+# import pandas as pd
 from share_data import dict
 
-
+# stock input & accessing dictionary
 stock_name = input('Enter stock name: ')
+
+# setting alert price
+alert = float(input('Enter the alert: '))
+
+# Checking given name in dictionary for actual name
 stock_exchange_name = dict[stock_name]
 
+# calling the site and giving input like stock name and .
 url = 'https://www.google.com/finance/quote/' + stock_exchange_name
-alert = float(input('Enter the alert: '))
 
 # Request the page
 page = requests.get(url)
@@ -21,15 +26,21 @@ tree = html.fromstring(page.content)
 prices = tree.xpath(
     '//div[@class="YMlKec fxKbKc"]/text()')
 # print(prices)
+
+# by default there is rupee symbol in front of the price (ex: ₹238.20), below command is to remove the symbol
 price = prices[0]
 price = float(price[3:])
-# print('This vallue', price[3:])
-# price= 405
-if alert > price:
-    print('price is below', alert, 'Current price is', price)
+
+# Comparing the alert price and the actual price.
+if alert >= price:
+    print('Stock price is below', alert, ',  Current price is', price)
+    # print('Stock price is below', alert, 'the current price is', price) # uncomment to test
+# below code is for saving the data to csv, only if the alert price is less than or equal to current price
+    # df = pd.DataFrame([{'Stock name': stock_exchange_name, 'Current Price': price, 'Alert price': alert}])
+    # df.to_csv('report.csv', index=False, encoding='utf-8')
 
 else:
-    print('Price is above', alert, 'Current price is', price)
+    print('Stock Price is above', alert, 'the current price is', price)
 
 # df = pd.DataFrame([{'Price': price}])
 # df.to_csv('sneakers.csv', index=False, encoding='utf-8')
