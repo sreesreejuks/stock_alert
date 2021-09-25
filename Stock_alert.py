bb319585ed6ba995bd41eaef3ce3d505afda4754
Sreeju KS <74912840+sreesreejuks@users.noreply.github.com>
2021-09-25 20:20:00 +0530

    Add files via upload

---
+++ Stock_alert.py
@@ -0,0 +1,32 @@
+# Import required modules
+from lxml import html
+import requests
+import pandas as pd
+
+stock_name= input('Enter stock name: ')
+
+url= 'https://www.google.com/finance/quote/'+stock_name
+alert= float(input('Enter the alert: '))
+
+# Request the page
+page = requests.get(url)
+
+# Parsing the page
+tree = html.fromstring(page.content)
+
+# Get element using XPath
+prices = tree.xpath(
+    '//div[@class="YMlKec fxKbKc"]/text()')
+#print(prices)
+price= prices[0]
+price=float(price[3:])
+#print('This vallue', price[3:])
+#price= 405
+if alert > price:
+    print('price is below', alert, 'Current price is', price)
+
+else:
+    print('Price is above', alert, 'Current price is',price)
+
+#df = pd.DataFrame([{'Price': price}])
+#df.to_csv('sneakers.csv', index=False, encoding='utf-8')
